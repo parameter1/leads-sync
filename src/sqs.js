13c314b462a196018e79187a0453712569f4f060
Jacob Bare <jacob@limit0.io>
2020-09-05 09:21:40 -0500

    only use env vars for sqs when not in lambda

--- src/sqs.js
+++ src/sqs.js
@@ -1,7 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const AWS = require('aws-sdk');
-const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION } = require('../env');
+const {
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY,
+  AWS_REGION,
+  AWS_EXECUTION_ENV,
+} = require('./env');
 
-module.exports = new AWS.SQS({
+// if there's a lambda execution env, do not use the local env vars when created the sqs client
+module.exports = AWS_EXECUTION_ENV ? new AWS.SQS({ apiVersion: '2012-11-05' }) : new AWS.SQS({
   apiVersion: '2012-11-05',
   accessKeyId: AWS_ACCESS_KEY_ID,
   secretAccessKey: AWS_SECRET_ACCESS_KEY,
