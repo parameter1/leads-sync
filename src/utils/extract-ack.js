b2efad8483a1f11c934ea238c4186fa78a416792
Jacob Bare <jacob@parameter1.com>
2021-02-09 09:46:30 -0600

    Support URL ID + acknowledgement query param
    Via `lt.lidack={id}~{ack}`

--- src/utils/extract-ack.js
+++ src/utils/extract-ack.js
@@ -1,5 +1,6 @@
 const pattern1 = /ack=([a-zA-Z0-9-_]+)/;
 const pattern2 = /lt\.ack=([a-zA-Z0-9-_]+)/;
+const pattern3 = /lt\.lidack=([a-f0-9]{24})~([a-zA-Z0-9-_]+)/;
 
 /**
  * Extracts the URL acknowledgement short code from a marketing cloud link.
@@ -7,10 +8,14 @@ const pattern2 = /lt\.ack=([a-zA-Z0-9-_]+)/;
  * Supports:
  * - `ack=r1NKYklEP`
  * - `lt.ack=r1NKYklEP`
+ * - `lt.lidack=601d65c194e7cf49c15896cf~r1NKYklEP`
  *
  * @param {string} value
  */
 module.exports = (value) => {
+  const combinedMatches = pattern3.exec(value);
+  if (combinedMatches && combinedMatches[2]) return combinedMatches[2];
+
   const legacyMatches = pattern1.exec(value);
   if (legacyMatches && legacyMatches[1]) return legacyMatches[1];
   const matches = pattern2.exec(value);
--- src/utils/extract-url-id.js
+++ src/utils/extract-url-id.js
@@ -1,5 +1,6 @@
 const pattern1 = /leads\.limit0\.io\/click\/([a-f0-9]{24})/;
 const pattern2 = /lt\.lid=([a-f0-9]{24})/;
+const pattern3 = /lt\.lidack=([a-f0-9]{24})~([a-zA-Z0-9-_]+)/;
 
 /**
  * Extracts the internal, URL ID (ExtractedUrl._id) from a marketing cloud link.
@@ -7,11 +8,15 @@ const pattern2 = /lt\.lid=([a-f0-9]{24})/;
  * Supports:
  * - `leads.limit0.io/click/{id}`
  * - `lt.id={id}`
+ * - `lt.lidack={id}~{ack}
  * where `{id}` is a 24-digit hex string
  *
  * @param {string} value
  */
 module.exports = (value) => {
+  const combinedMatches = pattern3.exec(value);
+  if (combinedMatches && combinedMatches[1]) return combinedMatches[1];
+
   const legacyMatches = pattern1.exec(value);
   if (legacyMatches && legacyMatches[1]) return legacyMatches[1];
   const matches = pattern2.exec(value);
